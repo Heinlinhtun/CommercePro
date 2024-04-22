793fcafa69934cf856a10f2ecc09332a279b01cd
Hein Lin Htun <49217071+Heinlinhtun@users.noreply.github.com>
2024-04-22 23:31:23 +0630

    Model Added

---
+++ CommercePro/BusinessLogic.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+
+namespace CommercePro
+{
+    [Table("Product")]
+    public class Product
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("productid")]
+        public int ProductID { get; set; }
+        [Column("name")]
+        public string? ProductName { get; set; }
+        [Column("model")]
+        public string? ProductModel { get; set; }
+
+        [Column("catid")]
+        public string? CategoryID { get; set; }
+        [Column("unitprice")]
+        public string? UnitPrice { get; set; }
+        [Column("quantity")]
+        public string? QuantityInStock { get; set; }
+    }
+
+    [Table("categories")]
+    public class Categories
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("catid")]
+        public int CategoryID { get; set; }
+    }
+
+}
---
+++ CommercePro/LocalDbService.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercePro
+{
+    public class LocalDbService
+    {
+        private const string DB_NAME = "demo_local_db.db3";
+        private readonly SQLiteAsyncConnection _connection;
+
+        public LocalDbService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+            _connection.CreateTableAsync<Product>();
+        }
+    }
+}
